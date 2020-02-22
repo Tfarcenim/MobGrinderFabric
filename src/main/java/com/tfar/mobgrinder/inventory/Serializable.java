2a90bf35984a43a662d6d45b9aaccd7578719fda
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-02-21 23:05:33 -0500

    1.2

--- src/main/java/com/tfar/mobgrinder/inventory/Serializable.java
+++ src/main/java/com/tfar/mobgrinder/inventory/Serializable.java
@@ -1,8 +1,6 @@
 package com.tfar.mobgrinder.inventory;
 
-import net.minecraft.nbt.CompoundTag;
-
 public interface Serializable<T> {
 	T serialize();
-	void deserialize(CompoundTag tag);
+	void deserialize(T t);
 }
--- src/main/java/com/tfar/mobgrinder/inventory/SerializableBasicInventory.java
+++ src/main/java/com/tfar/mobgrinder/inventory/SerializableBasicInventory.java
@@ -13,25 +13,25 @@ public class SerializableBasicInventory extends BasicInventory implements Serial
 
 	@Override
 	public CompoundTag serialize() {
-		ListTag nbtTagList = new ListTag();
+		ListTag listTag = new ListTag();
 		for (int i = 0; i < getInvSize(); i++) {
 			if (!getInvStack(i).isEmpty()) {
 				CompoundTag itemTag = new CompoundTag();
 				itemTag.putInt("Slot", i);
 				getInvStack(i).toTag(itemTag);
-				nbtTagList.add(itemTag);
+				listTag.add(itemTag);
 			}
 		}
 		CompoundTag nbt = new CompoundTag();
-		nbt.put("Items", nbtTagList);
+		nbt.put("Items", listTag);
 		return nbt;
 	}
 
 	@Override
-	public void deserialize(CompoundTag nbt) {
-		ListTag tagList = nbt.getList("Items", 10);
-		for (int i = 0; i < tagList.size(); i++) {
-			CompoundTag itemTags = tagList.getCompound(i);
+	public void deserialize(CompoundTag tag) {
+		ListTag listTag = tag.getList("Items", 10);
+		for (int i = 0; i < listTag.size(); i++) {
+			CompoundTag itemTags = listTag.getCompound(i);
 			int slot = itemTags.getInt("Slot");
 			if (slot >= 0 && slot < getInvSize()) {
 				setInvStack(slot, ItemStack.fromTag(itemTags));
